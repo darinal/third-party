d2ec7538fd394c209460e4ab853be6d378b0e0eb
Daryna Lysenko <daryna.lysenko@vivint.com>
2021-02-25 08:57:03 +0200

    added error generation

---
+++ src/api.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+
+const router = express.Router();
+
+let MODE = 0;
+
+router.get('/getMode', (req, res) => {
+    res.json({"mode": MODE});
+});
+
+router.post('/updateMode', (req, res) => {
+    const number = +req.body.toString();
+    if(number === 0 || number === 1){
+        MODE = number;
+    }
+    console.log(MODE);
+    res.json({"mode": MODE});
+});
+
+router.put('/doAddressCheck', (req, res) => {
+    if (MODE === 1) {
+        wait();
+        res.json({ "action": "timeout" });
+    }
+    else {        
+        res.json({"action": error})
+    }    
+});
+
+
+const wait = () => {
+    const seconds = 7;
+    var waitTill = new Date(new Date().getTime() + seconds * 1000);
+    console.log(waitTill); 
+    while(waitTill > new Date()){}
+ }
+
+app.use('/.netlify/functions/api', router);
+
+
+module.exports.handler = serverless(app);
